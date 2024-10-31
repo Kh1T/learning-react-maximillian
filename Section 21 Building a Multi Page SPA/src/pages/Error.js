4bf85ae54d7f548ab332955cef6ab72cb60f9cf6
Khit Khom <khomkhit460@gmail.com>
2024-10-31 11:13:28 +0700

    331

---
+++ Section 21 Building a Multi Page SPA/src/pages/Error.js
@@ -0,0 +1,14 @@
+import MainNavigation from "../components/MainNavigation";
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <MainNavigation />
+      <main>
+        <h1>Error Page</h1>
+      </main>
+    </div>
+  );
+};
+
+export default ErrorPage;
