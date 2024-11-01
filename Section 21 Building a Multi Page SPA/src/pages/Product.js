0c3dd9e70a8308540eb7a0fbe53f8db67732c303
Khit Khom <khomkhit460@gmail.com>
2024-11-01 14:25:35 +0700

    335

--- Section 21 Building a Multi Page SPA/src/pages/Product.js
+++ Section 21 Building a Multi Page SPA/src/pages/Product.js
@@ -1,9 +1,32 @@
+import { Link } from "react-router-dom";
+
+const DUMMY_PRODUCTS = [
+  {
+    productId: "p1",
+    title: "Product 1",
+  },
+  {
+    productId: "p2",
+    title: "Product 2",
+  },
+  { productId: "p3", title: "Product 3" },
+];
 const ProductPage = () => {
-    return (
-        <div>
-            <h1>Product Page</h1>
-        </div>
-    );  
-}
+  return (
+    <div>
+      <h1>Product Page</h1>
+      <ul>
+        {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.productId}>
+            <Link to={`/products/${product.productId}`}>{product.title}</Link>
+          </li>
+        ))}
+        <li>
+          <Link></Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default ProductPage;
