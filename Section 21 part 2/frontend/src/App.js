e0f2cadc82d09878690c92734bb9829b84c3aa87
Khit Khom <khomkhit460@gmail.com>
2024-11-05 10:03:37 +0700

    349

--- Section 21 part 2/frontend/src/App.js
+++ Section 21 part 2/frontend/src/App.js
@@ -29,11 +29,13 @@ import NewEventPage from "./pages/NewEvent.js";
 import EditEventPage from "./pages/EditEvent.js";
 import RootLayout from "./pages/Root.js";
 import EventsRootLayout from "./pages/EventsRoot.js";
+import ErrorPage from "./pages/Error.js";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {
