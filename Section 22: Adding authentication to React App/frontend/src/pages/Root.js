fe0f0acf76ab935543ed68da8d5eb059e45f027a
Khit Khom <khomkhit460@gmail.com>
2024-11-06 11:22:41 +0700

    380

--- Section 22: Adding authentication to React App/frontend/src/pages/Root.js
+++ Section 22: Adding authentication to React App/frontend/src/pages/Root.js
@@ -2,6 +2,7 @@ import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
 
 import MainNavigation from "../components/MainNavigation";
 import { useEffect } from "react";
+import { getTokenExpiration } from "../utils/auth";
 
 function RootLayout() {
   const submit = useSubmit();
@@ -10,9 +11,16 @@ function RootLayout() {
     if (!token) {
       return;
     }
+
+    if (token === "EXPIRED") {
+      submit(null, { action: "/logout", method: "POST" });
+      return;
+    }
+
+    const tokenDuration = getTokenExpiration();
     setTimeout(() => {
       submit(null, { action: "/logout", method: "POST" });
-    }, 1 * 60 * 60 * 1000);
+    }, tokenDuration);
   }, [token, submit]);
 
   return (
