3e040c62c17ad1aa5254d4ce350350f50bf26188
Khit Khom <khomkhit460@gmail.com>
2024-11-12 16:19:16 +0700

    437. Loading & Rendering Meal Details via Dynamic Routes & Route Parameters

--- Section 25 part 2/lib/meals.js
+++ Section 25 part 2/lib/meals.js
@@ -3,7 +3,14 @@ import sql from "better-sqlite3";
 const db = sql("meals.db");
 
 export async function getMeals() {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-  throw new Error('Loading Meals Failed')
-  return db.prepare("SELECT * from meals").all();
+  try {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    return db.prepare("SELECT * from meals").all();
+  } catch (error) {
+    throw new Error("Loading Meals Failed");
+  }
+}
+
+export function getMeal(slug) {
+  return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
 }
