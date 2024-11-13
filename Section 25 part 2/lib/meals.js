96f3a3bb55870354cc3083f5900f62a7c65b7a46
Khit Khom <khomkhit460@gmail.com>
2024-11-13 17:52:26 +0700

    446

--- Section 25 part 2/lib/meals.js
+++ Section 25 part 2/lib/meals.js
@@ -1,3 +1,5 @@
+import fs from "node:fs";
+
 import sql from "better-sqlite3";
 import slugify from "slugify";
 import xss from "xss";
@@ -20,4 +22,10 @@ export function getMeal(slug) {
 export function saveMeals(meal) {
   slug = slugify(meal.title, { lower: true });
   instructions = xss(meal.instructions);
+
+  const extension = meal.image.split(".").pop();
+  const fileName = `${meal.slug}.${extension}`;
+
+  const stream = fs.createWriteStream(`public/images/${fileName}`);
+  const bufferedImage = stream.write();
 }
